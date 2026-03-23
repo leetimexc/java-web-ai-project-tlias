e1bb86fa2456e1253d196aa43da0cba831c8a9c3
li-shuai <506507881@qq.com>
2026-03-24 05:37:50 +0800

    feat: 封装

--- tlias-web-management/src/main/java/com/example/mapper/DeptMapper.java
+++ tlias-web-management/src/main/java/com/example/mapper/DeptMapper.java
@@ -2,6 +2,8 @@ package com.example.mapper;
 
 import com.example.pojo.Dept;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -11,8 +13,18 @@ public interface DeptMapper {
 
     /**
      * 查询所有部门信息
-     * @return
      */
+    // 方法一：手动映射字段 @Results及@Result
+    //    @Results({
+    //            @Result(property = "createTime", column = "create_time"),
+    //            @Result(property = "updateTime", column = "update_time")
+    //    })
+    //    @Select("select id,name,create_time,update_time from dept order by update_time desc")
+
+    // 方法二：起别名 create_time createTime
+    //    @Select("select id,name,create_time createTime,update_time updateTime from dept order by update_time desc")
+
+    // 方法三（终极方案）：开启驼峰命名 mybatis.configuration.map-underscore-to-camel-case=true
     @Select("select id,name,create_time,update_time from dept order by update_time desc")
     List<Dept> findAll();
 }
