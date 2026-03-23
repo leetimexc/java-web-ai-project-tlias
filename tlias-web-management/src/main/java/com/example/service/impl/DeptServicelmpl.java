e913f740ba12c2b5e6e5c62dce9003f6edeb9b18
li-shuai <506507881@qq.com>
2026-03-24 02:51:11 +0800

    feat: 工程搭建

---
+++ tlias-web-management/src/main/java/com/example/service/impl/DeptServicelmpl.java
@@ -0,0 +1,8 @@
+package com.example.service.impl;
+
+import com.example.service.DeptService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeptServicelmpl implements DeptService {
+}
