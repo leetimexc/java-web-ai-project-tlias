c1dbcf26c2c792fe83c6201419101d678b057333
li-shuai <506507881@qq.com>
2026-04-03 23:27:23 +0800

    feat: 分页功能

--- tlias-web-management/src/main/java/com/example/controller/EmpController.java
+++ tlias-web-management/src/main/java/com/example/controller/EmpController.java
@@ -1,18 +1,31 @@
 package com.example.controller;
 
+import com.example.pojo.PageResult;
+import com.example.pojo.Result;
 import com.example.service.EmpService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 员工管理
  */
 @Slf4j
+@RequestMapping("/emp")
 @RestController
 public class EmpController {
 
     @Autowired
     private EmpService empService;
 
+    @GetMapping
+    public Result page(@RequestParam(defaultValue = "1") Integer page ,
+                       @RequestParam(defaultValue = "10") Integer pageSize){
+        log.info("分页查询，参数：page={}，pageSize={}", page, pageSize);
+        PageResult pageResult = empService.page(page, pageSize);
+        return Result.success(pageResult);
+    }
 }
--- tlias-web-management/src/main/java/com/example/service/EmpService.java
+++ tlias-web-management/src/main/java/com/example/service/EmpService.java
@@ -1,4 +1,12 @@
 package com.example.service;
 
+import com.example.pojo.PageResult;
+
 public interface EmpService {
+    /**
+     * 分页查询
+     * @param page 页码
+     * @param pageSize 每页记录数
+     */
+    PageResult page(Integer page, Integer pageSize);
 }
