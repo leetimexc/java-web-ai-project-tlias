ecd552b4419c079c27f5ffd9c6e09a3fb940e7c2
li-shuai <506507881@qq.com>
2026-04-04 02:52:38 +0800

    feat: 查询员工接口

--- tlias-web-management/src/main/java/com/example/mapper/EmpMapper.java
+++ tlias-web-management/src/main/java/com/example/mapper/EmpMapper.java
@@ -9,8 +9,14 @@ import java.util.List;
 @Mapper
 public interface EmpMapper {
     /**
+     * 查询总记录数
+     */
+    @Select("select count(*) from emp e left join dept d on e.dept_id = d.id ")
+    public Long count();
+
+    /**
      * 查询所有的员工及其对应的部门名称
      */
-    @Select("select e.*, d.name as deptName from emp e left join dept d on e.dept_id = d.id")
-    public List<Emp> list();
+    @Select("select e.*, d.name deptName from emp as e left join dept as d on e.dept_id = d.id limit #{start}, #{pageSize}")
+    public List<Emp> list(Integer start , Integer pageSize);
 }
--- tlias-web-management/src/main/java/com/example/service/impl/EmpServiceImpl.java
+++ tlias-web-management/src/main/java/com/example/service/impl/EmpServiceImpl.java
@@ -1,9 +1,13 @@
 package com.example.service.impl;
 
 import com.example.mapper.EmpMapper;
+import com.example.pojo.Emp;
+import com.example.pojo.PageResult;
 import com.example.service.EmpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 员工管理
@@ -13,5 +17,16 @@ public class EmpServiceImpl implements EmpService {
 
     @Autowired
     private EmpMapper empMapper;
+
+    @Override
+    public PageResult page(Integer page, Integer pageSize) {
+        //1. 获取总记录数
+       Long total = empMapper.count();
+       //2. 获取分页查询结果列表
+        Integer start = (page -1) * pageSize;
+        List<Emp> empList = empMapper.page(start, pageSize);
+        //3. 封装PageResult对象并返回
+        return new PageResult(total, empList);
+    }
 
 }
