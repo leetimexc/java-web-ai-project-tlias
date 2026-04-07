9f5ea627740730b14abf43924774a2b70beb3370
li-shuai <506507881@qq.com>
2026-04-08 03:45:14 +0800

    feat: 修复引入时间类

--- tlias-web-management/src/main/java/com/example/controller/EmpController.java
+++ tlias-web-management/src/main/java/com/example/controller/EmpController.java
@@ -5,6 +5,7 @@ import com.example.pojo.Result;
 import com.example.service.EmpService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,8 +27,10 @@ public class EmpController {
     @GetMapping
     public Result page(@RequestParam(defaultValue = "1") Integer page ,
                        @RequestParam(defaultValue = "10") Integer pageSize,
-                       String name, Integer gender, LocalDate begin, LocalDate end){
-        log.info("分页查询，参数：page={}，pageSize={}", page, pageSize);
+                       String name, Integer gender,
+                       @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+                       @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+        log.info("分页查询，参数：{},{},{},{},{}", page, pageSize, name, gender, begin, end);
 //        PageResult pageResult = empService.page(page, pageSize);
         PageResult pageResult = empService.page(page, pageSize, name, gender, begin, end);
         return Result.success(pageResult);
--- tlias-web-management/src/main/java/com/example/service/impl/EmpServiceImpl.java
+++ tlias-web-management/src/main/java/com/example/service/impl/EmpServiceImpl.java
@@ -9,6 +9,7 @@ import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
