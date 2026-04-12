f2c7eae4949ea08e0ff428ce5643556950bea64b
li-shuai <506507881@qq.com>
2026-04-13 05:51:42 +0800

    feat: 使用PageHelper实现

--- tlias-web-management/src/main/java/com/example/mapper/EmpMapper.java
+++ tlias-web-management/src/main/java/com/example/mapper/EmpMapper.java
@@ -11,16 +11,23 @@ import java.util.List;
  */
 @Mapper
 public interface EmpMapper {
+
+    // ------------------------------------- 1. 原始分页查询实现
     /**
      * 查询员工数量
      */
-    @Select("select count(*) from emp e left join dept d on e.dept_id = d.id;")
-    public long count();
+    // @Select("select count(*) from emp e left join dept d on e.dept_id = d.id;")
+    // public long count();
 
     /**
      * 分页查询
      */
-    @Select("select e.*, d.name deptName from emp e left join dept d on e.dept_id = d.id " +
-            "order by e.update_time desc limit #{start},#{pageSize};")
-    public List<Emp> list(Integer start, Integer pageSize);
+    //  @Select("select e.*, d.name deptName from emp e left join dept d on e.dept_id = d.id " +
+    //     "order by e.update_time desc limit #{start},#{pageSize};")
+    //  public List<Emp> list(Integer start, Integer pageSize);
+
+    // ------------------------------------- 2. 使用PageHelper依赖
+    @Select("select e.*, d.name deptName from emp e left join dept d on e.dept_id = d.id order by e.update_time desc")
+    public List<Emp> list();
+
 }
--- tlias-web-management/src/main/java/com/example/service/impl/EmpServiceImpl.java
+++ tlias-web-management/src/main/java/com/example/service/impl/EmpServiceImpl.java
@@ -4,6 +4,8 @@ import com.example.mapper.EmpMapper;
 import com.example.pojo.Emp;
 import com.example.pojo.PageResult;
 import com.example.service.EmpService;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,16 +17,39 @@ public class EmpServiceImpl implements EmpService {
     @Autowired
     private EmpMapper empMapper;
 
+    /**
+     * 1. 原始分页查询
+     */
+    //    @Override
+    //    public PageResult<Emp> page(Integer page, Integer pageSize) {
+    //        // 1. 调用mapper接口，查询总记录数
+    //        Long total = empMapper.count();
+    //
+    //        // 2. 调用mapper接口，查询分页数据
+    //        Integer start = (page - 1) * pageSize;
+    //        List<Emp> rows = empMapper.list(start, pageSize);
+    //
+    //        // 3. 封装结果 PageResult
+    //        return new PageResult<Emp>(total, rows);
+    //    }
+
+    /**
+     * 2. 引入PageHelper依赖实现分页查询
+     * 使用PageHelper注意事项：
+     * 1. 定义的SQL语句结尾不能加分号；
+     * 2. PageHelper 仅仅能对紧跟在其后的第一个查询语句进行分页处理
+     */
     @Override
     public PageResult<Emp> page(Integer page, Integer pageSize) {
-        // 1. 调用mapper接口，查询总记录数
-        Long total = empMapper.count();
+        // 1. 设置分页参数（PageHelper）
+        PageHelper.startPage(page, pageSize);
 
-        // 2. 调用mapper接口，查询分页数据
-        Integer start = (page - 1) * pageSize;
-        List<Emp> rows = empMapper.list(start, pageSize);
+        // 2. 执行查询（PageHelper）
+        List<Emp> empList = empMapper.list();
+        // tips: 这里对应上面的注意事项2，后面再加不会自动添加分页处理功能
 
-        // 3. 封装结果 PageResult
-        return new PageResult<Emp>(total, rows);
+        // 3. 解析查询结果，并封住
+        Page<Emp> p = (Page<Emp>) empList;
+        return new PageResult<Emp>(p.getTotal(), p.getResult());
     }
 }
