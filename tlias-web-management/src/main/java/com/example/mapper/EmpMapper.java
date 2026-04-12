ddcc8ecee5ba298a15042d451d8590f319d814f7
li-shuai <506507881@qq.com>
2026-04-12 22:42:00 +0800

    feat: 查询员工总记录数

--- tlias-web-management/src/main/java/com/example/mapper/EmpMapper.java
+++ tlias-web-management/src/main/java/com/example/mapper/EmpMapper.java
@@ -1,11 +1,17 @@
 package com.example.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * 员工信息
  */
 @Mapper
 public interface EmpMapper {
+    /**
+     * 查询员工数量
+     */
+    @Select("select count(*) from emp e left join dept d on e.dept_id = d.id;")
+    public long count();
 
 }
