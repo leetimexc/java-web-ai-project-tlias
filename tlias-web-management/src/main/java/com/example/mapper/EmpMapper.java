900b21c019b4042a862069bd06fc4f379129b25d
li-shuai <506507881@qq.com>
2026-04-15 05:56:01 +0800

    feat: 员工条件分页查询&使用xml文件&concat拼接

--- tlias-web-management/src/main/java/com/example/mapper/EmpMapper.java
+++ tlias-web-management/src/main/java/com/example/mapper/EmpMapper.java
@@ -28,6 +28,6 @@ public interface EmpMapper {
     //  public List<Emp> list(Integer start, Integer pageSize);
 
     // ------------------------------------- 2. 使用PageHelper依赖
-    @Select("select e.*, d.name deptName from emp e left join dept d on e.dept_id = d.id order by e.update_time desc")
+    // @Select("select e.*, d.name deptName from emp e left join dept d on e.dept_id = d.id order by e.update_time desc")
     public List<Emp> list(String name, Integer gender, LocalDate begin, LocalDate end);
 }
