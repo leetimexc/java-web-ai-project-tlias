07d776d6164ecf8b3e123d62e682f5bd1eb1397c
li-shuai <506507881@qq.com>
2026-04-25 21:56:45 +0800

    feat: 事务特性

--- tlias-web-management/src/main/java/com/example/service/impl/EmpLogServicelmpl.java
+++ tlias-web-management/src/main/java/com/example/service/impl/EmpLogServicelmpl.java
@@ -5,6 +5,8 @@ import com.example.pojo.EmpLog;
 import com.example.service.EmpLogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EmpLogServicelmpl implements EmpLogService {
@@ -12,6 +14,8 @@ public class EmpLogServicelmpl implements EmpLogService {
     @Autowired
     private EmpLogMapper empLogMapper;
 
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW) //需要在一个新的事务中运行
     @Override
     public void insert(EmpLog empLog) {
         empLogMapper.insert(empLog);
