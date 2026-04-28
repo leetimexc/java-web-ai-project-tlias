998b97d2074855f94dd29eef1288509c1714a1f4
li-shuai <506507881@qq.com>
2026-04-29 00:02:12 +0800

    feat: 完成文件本地存储

--- tlias-web-management/src/main/java/com/example/controller/UploadController.java
+++ tlias-web-management/src/main/java/com/example/controller/UploadController.java
@@ -6,13 +6,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 @Slf4j
 @RestController
 public class UploadController {
 
     @PostMapping("/upload")
-    public Result upload(String name, Integer age, MultipartFile file){
+    public Result upload(String name, Integer age, MultipartFile file) throws IOException {
         log.info("接受参数：{},{},{}", name, age, file);
+        // 获取原始文件名
+        String originalFilename = file.getOriginalFilename();
+        // 保存文件
+        file.transferTo(new File("Users/li_shuai/我的代码/后端学习/Java项目实战/java-web-ai-project-tlias/tlias-web-management/images/"+originalFilename));
         return Result.success();
     }
 }
