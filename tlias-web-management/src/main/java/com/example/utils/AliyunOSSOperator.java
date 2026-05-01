57b22e6ca61c6bf9233fdf457b4e2fab51d43d20
li-shuai <506507881@qq.com>
2026-05-01 18:08:02 +0800

    feat: @ConfigurationProperties批量处理变量

--- tlias-web-management/src/main/java/com/example/utils/AliyunOSSOperator.java
+++ tlias-web-management/src/main/java/com/example/utils/AliyunOSSOperator.java
@@ -4,6 +4,7 @@ import com.aliyun.oss.*;
 import com.aliyun.oss.common.auth.CredentialsProviderFactory;
 import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
 import com.aliyun.oss.common.comm.SignVersion;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.io.ByteArrayInputStream;
@@ -14,14 +15,26 @@ import java.util.UUID;
 @Component
 public class AliyunOSSOperator {
 
+    // 方式一： 通过@Value注解一个属性一个属性的注入
+    /*
     @Value("${aliyun.oss.endpoint}")
     private String endpoint;
     @Value("${aliyun.oss.bucketName}")
     private String bucketName;
     @Value("${aliyun.oss.region}")
     private String region;
+    */
+
+    // 方式二： 通过@ConfigurationProperties注解批量注入
+    @Autowired
+    private AliyunOSSProperties aliyunOSSProperties;
+
 
     public String upload(byte[] content, String originalFilename) throws Exception {
+        String endpoint = aliyunOSSProperties.getEndpoint();
+        String bucketName = aliyunOSSProperties.getBucketName();
+        String region = aliyunOSSProperties.getRegion();
+
         // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
         EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
 
