478f1d4298c6bd5cef1c0541e8991327bf52b349
li-shuai <506507881@qq.com>
2026-05-07 05:48:22 +0800

    docs: jwt 组成介绍

--- tlias-web-management/src/test/java/com/example/JwtTest.java
+++ tlias-web-management/src/test/java/com/example/JwtTest.java
@@ -32,6 +32,12 @@ public class JwtTest {
      */
     @Test
     public void testParseJwt() {
+        /*
+         JWT的组成： （JWT令牌由三个部分组成，三个部分之间使用英文的点来分割）
+            - 第一部分：Header(头）， 记录令牌类型、签名算法等。 例如：{"alg":"HS256","type":"JWT"}
+            - 第二部分：Payload(有效载荷），携带一些自定义信息、默认信息等。 例如：{"id":"1","username":"Tom"}
+            - 第三部分：Signature(签名），防止Token被篡改、确保安全性。将header、payload，并加入指定秘钥，通过指定签名算法计算而来。
+         */
         String token = "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJhZG1pbiIsInN1YiI6InRsaWFzIiwiZXhwIjoxNzc4MTA2NzU4fQ.z-CCyOO7rhdjr1skQoZCwBrpL6qa-o0kJotqpOnTR14";
         Claims claims = Jwts.parser().setSigningKey("dGxpYXM=")
                 .parseClaimsJws(token)
