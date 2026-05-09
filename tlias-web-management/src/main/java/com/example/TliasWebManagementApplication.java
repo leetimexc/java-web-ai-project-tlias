9c18cc5bbb7adce53a967383db55b7c90a1bf2e8
li-shuai <506507881@qq.com>
2026-05-09 13:45:08 +0800

    feat: 过滤器Filter

--- tlias-web-management/src/main/java/com/example/TliasWebManagementApplication.java
+++ tlias-web-management/src/main/java/com/example/TliasWebManagementApplication.java
@@ -2,7 +2,9 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 
+@ServletComponentScan // 开启了SpringBoot的Servlet组件扫描功能
 @SpringBootApplication
 public class TliasWebManagementApplication {
 
---
+++ tlias-web-management/src/main/java/com/example/filter/DemoFilter.java
@@ -0,0 +1,32 @@
+package com.example.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*") // 拦截所有请求
+@Slf4j
+public class DemoFilter implements Filter {
+
+    // 初始化方法，在服务器启动时执行一次
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("init 初始化方法...");
+    }
+
+    //拦截到请求之后，执行，会执行多次
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        log.info("doFilter 拦截到请求...");
+        // 放行
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    // 销毁方法，在服务器关闭时执行一次
+    @Override
+    public void destroy() {
+        log.info("destroy 销毁方法...");
+    }
+}
